ee757cb7ed7e14f509fa089f6ef075222a050251
Mohamedosama20 <mohamedosama667@yahoo.com>
2022-12-03 15:19:16 +0200

    Amazon

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,46 @@
+import './Hero.css'
+import heroImg from '../../assets/hero.png' ;
+import { RiShoppingBagFill } from 'react-icons/ri' ;
+import { BsArrowRight } from 'react-icons/bs' ;
+
+const Hero = () => {
+  return (
+    <div className='container'>
+        {/* left side */}
+        <div className='h-sides'>
+            <span className='text1'>Skin Protection Cream</span>
+            <div className='text2'>
+                <span>Trendy Collections</span>
+                <span>Seedily say has suitable disposal and boy. Exercise joy man children rejoiced.</span>
+            </div>
+        </div>
+        {/* middle side */}
+        <div className='wrapper'>
+            <div className='blueCircle'></div>
+            <img src={heroImg} alt='hero' width={600} />
+            <div className='cart2'>
+                <RiShoppingBagFill/>
+                <div className='signup'>
+                    <span>Best Signup Offers</span>
+                    <div>
+                        <BsArrowRight/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* right side */}
+        <div className='h-sides'>
+            <div className='traffic'>
+            <span>1.5m</span>
+            <span>Monthly Traffic</span>
+        </div>
+        <div className='customers'>
+            <span>100K</span>
+            <span>Happy Customers</span>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
