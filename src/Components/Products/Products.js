e7382102ed1adb18a1efdbc48ca22b4b3c23ab7f
Mohamedosama20 <mohamedosama667@yahoo.com>
2022-12-03 21:00:48 +0200

    amazon

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,8 +1,45 @@
 import './Products.css';
+import Plane from '../../assets/plane.png';
+import {ProductsData} from '../../data/products';
+import {useState} from 'react';
 
 const Products = () => {
+const [MenuProducts, setMenuProducts] = useState(ProductsData);
   return (
-    <div>Products</div>
+    <div className='p-container'>
+        <img src={Plane} alt='plane'/>
+        <h1>Our Featured Products</h1>
+        <div className='p-menu'>
+                <ul className='p-menu-items'>
+                    <li>All</li>
+                    <li>Skin Care</li>
+                    <li>Conditioners</li>
+                    <li>Foundations</li>
+                </ul>
+
+                <div className='p-list'>
+                {
+                    MenuProducts.map((product, i)=>( 
+                    <div className='product'>
+                        <div className='p-left'>
+                            <div className='name'>
+                                <span>
+                                    {product.name}
+                                </span>
+                                <span>
+                                {product.detail}
+                                </span>
+                            </div>
+                            <span>{product.price}$</span>
+                            <div>Show Now</div>
+                        </div>
+                        <img src={product.img} alt='product' className='p-img'/>
+                    </div>
+                    )) 
+                }
+                </div>
+            </div>
+        </div>
   )
 }
 
