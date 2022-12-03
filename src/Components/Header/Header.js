315e98c257ab4ae101e6b2eeffa45f073973f449
Mohamedosama20 <mohamedosama667@yahoo.com>
2022-12-03 14:06:10 +0200

    Amazon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,25 @@
 import './Header.css'
-
+import logo from '../../assets/logo.png' ;
+import { CgShoppingBag } from 'react-icons/cg' ;
 const Header = () => {
   return (
     <div className='container'>
         <div className='logo'>
-        Logo
+        <img src={logo} alt='logo' />
+        <span>Amazon</span>
         </div>
         <div className='right'>
         <div className='menu'>
+        <ul className='menu'>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>Eng</li>
+        </ul>
         </div>
         <input type='text' placeholder='Search' className='search'/>
-        <span>PNg icon</span>
+        <CgShoppingBag className='cart' />
         </div>
     </div>
   )
